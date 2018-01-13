3d23956928d79b571d8e6a330142a326556c4243
dickson <kr.d.tsl@gmail.com>
2018-01-13 16:01:30 -0500

    special instructions for customer

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20180113205409) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
+    t.string "special_instructions"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
