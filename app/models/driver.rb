698925682d0acfcb497514556d4dfa6095b26739
Dickson Lau <dickson@thelevelup.com>
2018-01-25 18:33:14 -0500

    add devise to driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,7 @@
 class Driver < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :cars
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  devise_for :drivers
   devise_for :customers
 end
